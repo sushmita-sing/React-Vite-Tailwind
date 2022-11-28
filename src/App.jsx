2b6fc540acdfc39775786090384754705a51cfd8
alturist-su <aimsushmitasingh@gmail.com>
2022-11-29 00:26:15 +0530

    billing and CardDeal completed..

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import styles from './style'
 import { Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from './components'
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
+  <div className="bg-primary w-full overflow-hidden ">
     <div className={`${styles.paddingX} ${styles.flecCenter}`}>
       <div className={`${styles.bandwidth}`}>
         <Navbar />
